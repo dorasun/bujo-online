7b2b515bdb645446c5afc5d781770236a2273b25
Dora Sun <lobster1216@gmail.com>
2017-11-02 23:27:07 -0400

    Committing edit to app.js

--- src/app.js
+++ src/app.js
@@ -15,12 +15,24 @@ const Event = mongoose.model('Event');
 const Note = mongoose.model('Note');
 
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
+const sessionOptions = {
+    secret: 'secret cookie thang (store this elsewhere!)',
+    resave: true,
+      saveUninitialized: true
+};
+app.use(session(sessionOptions));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
+	res.render('index');
 });
 
 app.listen(3000);
