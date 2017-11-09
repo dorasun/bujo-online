4131f506015f898a26d21692824e921f739b97d4
Dora Sun <lobster1216@gmail.com>
2017-11-09 14:10:04 -0500

    Committing a working form

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const app = express();
 
+const passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+
 require('./db.js');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const Page = mongoose.model('Page');
 const Year = mongoose.model('Year');
 const Month = mongoose.model('Month');
 const Week = mongoose.model('Week');
@@ -20,19 +23,172 @@ const session = require('express-session');
 const sessionOptions = {
     secret: 'secret cookie thang (store this elsewhere!)',
     resave: true,
-      saveUninitialized: true
+    saveUninitialized: true
 };
 app.use(session(sessionOptions));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-	res.render('index');
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		User.findOne({ username: username }, function(err, user) {
+			if (err) { return done(err); }
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username.' });
+			}
+			if (!user.validPassword(password)) {
+				return done(null, false, { message: 'Incorrect password.' });
+			}
+			return done(null, user);
+		});
+	}
+));
+
+app.get('/login', function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err) { return next(err); }
+		if (!user) { return res.redirect('/login'); }
+		req.logIn(user, function(err) {
+			if (err) { return next(err); }
+			return res.redirect('/users/' + user.username);
+		});
+	})(req, res, next);
 });
 
-app.listen(3000);
+app.post('/login',
+	passport.authenticate('local', { successRedirect: '/',
+	                               failureRedirect: '/login',
+	                               failureFlash: true })
+);
+
+app.get('/', function(req, res){
+	Page.find({}, function(err, pages) {
+		if(err){
+			console.log('No pages yet');
+			res.render('index', {});
+		}
+		else{
+			console.log(pages);
+			res.render('index', {pages: pages});
+		}
+	});
+});
+
+app.post('/page-add', function(req, res){	//for adding more links to the index
+	const newPage = new Page({
+		title: req.body.page_title,
+		items: []
+	});
+
+	newPage.save(function(err){
+		if (err){
+			error = 'Error has occured';
+			res.redirect('/');
+		}
+		else{
+			error = '';
+			res.redirect('/page/'+newPage.slug);
+		}
+	});
+
+	// User.find({}, function(err, user) {
+	// 	if(err){
+	// 		console.log("No User?????");
+	// 	}
+	// 	else{
+	// 		user.pages.push(newPage);
+	// 		user.save(function(err){
+	// 			if(err){
+	// 				console.log('Error saving');
+	// 			}
+	// 			else{
+	// 				res.redirect('/');
+	// 			}
+	// 		});
+	// 	}
+	// });
+});
+
+app.get('/page/:slug', function(req, res) {
+	Page.findOne({slug: req.params.slug}, function(err, page) {
+		if(err){
+			res.render('page', {});
+		}
+		else{
+			res.render('page', {page: page});
+		}
+	});
+});
+
+// app.post('/page/:slug-add', function(req, res){
+// 	let newItem;
+
+// 	if(req.body.type === 'task'){
+// 		newItem = new Task({
+// 			content: req.body.content
+// 		});
+// 	}
+// 	else if(req.body.type === 'event'){
+// 		newItem = new Event({
+// 			content: req.body.content
+// 		});
+// 	}
+// 	else if(req.body.type === 'note'){
+// 		newItem = new Note({
+// 			content: req.body.content
+// 		});
+// 	}
+
+// 	Page.findOne({slug: req.params.slug}, function(err, page) {
+// 		if(err){
+// 			console.log("No page found");
+// 		}
+// 		else{
+// 			console.log(page);
+// 			console.log(page.items);
+// 			if(req.body.type === 'task'){
+// 				if(!page.items.tasks){
+// 					page.items.tasks = [];
+// 				}
+// 				page.items.tasks.push(newItem);
+// 			}
+// 			else if(req.body.type === 'event'){
+// 				if(!page.items.events){
+// 					page.items.events = [];
+// 				}
+// 				page.items.events.push(newItem);
+// 			}
+// 			else if(req.body.type === 'note'){
+// 				console.log('NOTEE');
+// 				if(!page.items.notes){
+// 					page.items.notes = [];
+// 				}
+// 				page.items.notes.push(newItem);
+// 				console.log(page.items);
+// 			}
+
+// 			page.items.save(function(err){
+// 				if(err){
+// 					console.log('Error');
+// 				}
+// 				else{
+// 					page.save(function(err){
+// 						if(err){
+// 							console.log('Error');
+// 						}
+// 						else{
+// 							res.redirect('/page/'+page.slug);
+// 						}
+// 					});
+// 				}
+// 			});
+// 		}
+// 	});
+// });
+
+app.listen(3000);//process.env.PORT || 3000);
--- src/db.js
+++ src/db.js
@@ -1,69 +1,19 @@
-// first draft data model
+// second draft data model
 const mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
 
 /*
 	User
 	my site will require authentication, so users will have a username and password
 	users will have Year, Month, Week, and Day objects but those will be embedded
 	only years, months, weeks, and days with added data will be stored, and not empty years
+	users will also have pages, which represent additional notes outside date related ones
  */
 const User = new mongoose.Schema({
 	// username provided by authentication plugin
 	// password hash provided by authentication plugin
-	years: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Year' }],
-});
-
-/*
-	Year
-	represents a user's notes for the year
- */
-const Year = new mongoose.Schema({
-	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-	year: {type: Number, required: true},
-	items: {type: [Items], required: true},
-	months: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Month' }]
-});
-
-/*
-	Month
-	represents a user's notes for the month
- */
-const Month = new mongoose.Schema({
-	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-	month: {type: Number, required: true},
-	items: {type: [Items], required: true},
-	weeks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Week' }]
-});
-
-/*
-	Week
-	represents a user's notes for the week
- */
-const Week = new mongoose.Schema({
-	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-	week: {type: Number, required: true},
-	items: {type: [Items], required: true},
-	days: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Day' }]
-});
-
-/*
-	Day
-	represents a user's notes for the day
- */
-const Day = new mongoose.Schema({
-	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-	day: {type: Number, required: true},
-	items: {type: [Items], required: true},,
-});
-
-/*
-	Item
-	represents a list of items in a user's notes
- */
-const Item = new mongoose.Schema({
-	tasks: [Task],
-  	events: [Event], 
-  	notes: [Note]
+	pages: [{type: mongoose.Schema.Types.ObjectId, ref: 'Page'}],
+	years: [{type: mongoose.Schema.Types.ObjectId, ref: 'Year'}]
 });
 
 /*
@@ -93,7 +43,77 @@ const Note = new mongoose.Schema({
 	content: String
 });
 
+/*
+	Item
+	represents a list of items in a user's notes
+ */
+const Item = new mongoose.Schema({
+	tasks: [Task],
+  	events: [Event], 
+  	notes: [Note]
+});
+
+/*
+	Page
+	represents a page in a user's bujo that isn't time related
+ */
+const Page = new mongoose.Schema({
+	title: {type: String, required: true},
+	items: [Item]
+});
+Page.plugin(URLSlugs('title'));
+
+/*
+	Year
+	represents a user's notes for the year
+ */
+const Year = new mongoose.Schema({
+	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+	year: {type: Number, required: true},
+	items: {type: [Item], required: true},
+	months: [{type: mongoose.Schema.Types.ObjectId, ref: 'Month'}]
+});
+
+/*
+	Month
+	represents a user's notes for the month
+ */
+const Month = new mongoose.Schema({
+	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+	month: {type: Number, required: true},
+	items: {type: [Item], required: true},
+	weeks: [{type: mongoose.Schema.Types.ObjectId, ref: 'Week'}]
+});
+
+/*
+	Week
+	represents a user's notes for the week
+ */
+const Week = new mongoose.Schema({
+	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+	week: {type: Number, required: true},
+	items: {type: [Item], required: true},
+	days: [{type: mongoose.Schema.Types.ObjectId, ref: 'Day'}]
+});
+
+/*
+	Day
+	represents a user's notes for the day
+ */
+const Day = new mongoose.Schema({
+	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+	day: {type: Number, required: true},
+	items: {type: [Item], required: true}
+});
+
+
+Year.plugin(URLSlugs('year'));
+Month.plugin(URLSlugs('month'));
+Week.plugin(URLSlugs('week'));
+Day.plugin(URLSlugs('day'));
+
 mongoose.model('User', User);
+mongoose.model('Page', Page);
 mongoose.model('Year', Year);
 mongoose.model('Month', Month);
 mongoose.model('Week', Week);
@@ -102,3 +122,25 @@ mongoose.model('Item', Item);
 mongoose.model('Task', Task);
 mongoose.model('Event', Event);
 mongoose.model('Note', Note);
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+// let dbconf;
+// if (process.env.NODE_ENV === 'PRODUCTION') {
+// 	// if we're in PRODUCTION mode, then read the configration from a file
+// 	// use blocking file io to do this...
+// 	const fs = require('fs');
+// 	const path = require('path');
+// 	const fn = path.join(__dirname, 'config.json');
+// 	const data = fs.readFileSync(fn);
+
+// 	// our configuration file will be in json, so parse it and set the
+// 	// conenction string appropriately!
+// 	const conf = JSON.parse(data);
+// 	dbconf = conf.dbconf;
+// } else {
+// 	// if we're not in PRODUCTION mode, then use
+// 	dbconf = 'mongodb://localhost/YOUR_DATABASE_NAME_HERE';
+// }
+// mongoose.connect(dbconf);
+
+mongoose.connect('mongodb://localhost/final-temp');
